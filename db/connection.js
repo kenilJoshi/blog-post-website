c754248566a3edd3b169abe6643fb7953b6defef
Kenil joshi <82821607+kenilJoshi@users.noreply.github.com>
2023-01-09 23:43:48 +0530

    blog post website

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 
 
 const conn = mysql.createConnection({
-    host: "0.0.0.0",
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
