65a830f153ea79830658ab0ef2f95315233612f0
Kenil joshi <82821607+kenilJoshi@users.noreply.github.com>
2023-01-10 10:00:36 +0530

    blog post website

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -13,13 +13,21 @@ homeRoute.get('/', (req, res) => {
 
 homeRoute.post('/getUser', (req, res) => {
     console.log(req.body.title);
-    let sql = `SELECT * FROM railway.posts
+    if (req.body.title !== '') {
+        let sql = `SELECT * FROM railway.posts
     WHERE Title='${req.body.title}';`
-    conn.query(sql, function (err, result) {
-        if (err) throw err
+        conn.query(sql, function (err, result) {
+            if (err) throw err
 
-        res.render("../views/home", { results: result })
-    })
+            res.render("../views/home", { results: result })
+        })
+    } else if (req.body.title === '') {
+        conn.query("SELECT * FROM railway.posts;", function (err, result) {
+            if (err) throw err
+            res.render("../views/home", { results: result })
+
+        })
+    }
 })
 
 
